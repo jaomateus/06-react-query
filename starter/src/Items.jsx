0e58adb3d9e380326b44dee61942b4b084b1aaa4
Joao Mateus <jaomateus@gmail.com>
2023-04-25 10:34:15 +0100

    set form mutation

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -1,13 +1,27 @@
-import SingleItem from './SingleItem';
-import customFetch from './utils';
+import { useQuery } from "@tanstack/react-query";
+import SingleItem from "./SingleItem";
+import customFetch from "./utils";
 
 const Items = ({ items }) => {
-  return (
-    <div className='items'>
-      {items.map((item) => {
-        return <SingleItem key={item.id} item={item} />;
-      })}
-    </div>
-  );
+	const { isLoading, data, isError } = useQuery({
+		queryKey: ["tasks"],
+		queryFn: () => customFetch.get("/"),
+	});
+
+	if (isLoading) {
+		return <p style={{ marginTop: "1rem" }}>Loading...</p>;
+	}
+
+  if(isError){
+    return <p style={{ marginTop: "1rem" }}>There was an error...</p>;
+  }
+
+	return (
+		<div className='items'>
+			{data.data.taskList.map((item) => {
+				return <SingleItem key={item.id} item={item} />;
+			})}
+		</div>
+	);
 };
 export default Items;
